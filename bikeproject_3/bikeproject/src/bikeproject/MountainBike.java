fb174a70e61dfeea862e216b44fa5736dcf98d82
Narendra Yusuf <rendra.narendrayusuf@gmail.com>
2025-05-04 15:11:45 +0700

    Using the MountainParts interface with the MountainBike class adding any unimplemented methods required. Add the required internal code for each of the added methods.

--- bikeproject_3/bikeproject/src/bikeproject/MountainBike.java
+++ bikeproject_3/bikeproject/src/bikeproject/MountainBike.java
@@ -1,6 +1,6 @@
 package bikeproject;
 
-public class MountainBike extends Bike{
+public class MountainBike extends Bike implements MountainParts {
 
 	private String suspension, type;
 	private int frameSize;
@@ -24,4 +24,25 @@ public class MountainBike extends Bike{
 		System.out.println("This mountain bike is a " + this.type + " bike and has a " + this.suspension + " suspension and a frame size of " + this.frameSize + "inches.");
 		
 	}//end method printDescription
+	// Implementing methods from MountainParts interface
+	@Override
+	public String getSuspension() {
+		return this.suspension;
+	}//end method getSuspension
+
+	@Override
+	public void setSuspension(String newValue) {
+		this.suspension = newValue;
+	}//end method setSuspension
+
+	@Override
+	public String getType() {
+		return this.type;
+	}//end method getType
+
+	@Override
+	public void setType(String newValue) {
+		this.type = newValue;
+	}//end method setType
+
 }//end class MountainBike
